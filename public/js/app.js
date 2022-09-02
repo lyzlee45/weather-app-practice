3387a873d40fed5f8bc2d0dfa40835cd4cbacc9e
Mark Joseph C. Eboña <josephladosaw@gmail.com>
2022-09-02 17:53:14 +0800

    Init Commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+const weatherForm = document.querySelector("form");
+const searchElement = document.querySelector("input");
+const messageOne = document.querySelector("#message-one");
+const messageTwo = document.querySelector("#message-two");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  messageOne.textContent = "Loading....";
+  messageTwo.textContent = ""
+  const location = searchElement.value;
+
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          return (messageOne.textContent = data.error);
+        }
+
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+      });
+    }
+  );
+});
